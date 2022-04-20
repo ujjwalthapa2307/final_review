f050bec6cee35bb5692095d8bea7af01d01334d0
unknown <ujjwalthapa2307@gmail.com>
2022-04-20 11:46:40 -0400

    Generate UTM Contract Controller

---
+++ Consulting/Consulting/Controllers/UTMContractController.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Consulting.Models;
+
+namespace Consulting.Controllers
+{
+    public class UTMContractController : Controller
+    {
+        private readonly ConsultingContext _context;
+
+        public UTMContractController(ConsultingContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UTMContract
+        public async Task<IActionResult> Index()
+        {
+            var consultingContext = _context.Contract.Include(c => c.Customer);
+            return View(await consultingContext.ToListAsync());
+        }
+
+        // GET: UTMContract/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contract = await _context.Contract
+                .Include(c => c.Customer)
+                .FirstOrDefaultAsync(m => m.ContractId == id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            return View(contract);
+        }
+
+        // GET: UTMContract/Create
+        public IActionResult Create()
+        {
+            ViewData["CustomerId"] = new SelectList(_context.Customer, "CustomerId", "CustomerId");
+            return View();
+        }
+
+        // POST: UTMContract/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ContractId,CustomerId,Name,StartDate,DaysDuration,QuotedPrice,Closed,TotalChargedToDate")] Contract contract)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(contract);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customer, "CustomerId", "CustomerId", contract.CustomerId);
+            return View(contract);
+        }
+
+        // GET: UTMContract/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contract = await _context.Contract.FindAsync(id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customer, "CustomerId", "CustomerId", contract.CustomerId);
+            return View(contract);
+        }
+
+        // POST: UTMContract/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ContractId,CustomerId,Name,StartDate,DaysDuration,QuotedPrice,Closed,TotalChargedToDate")] Contract contract)
+        {
+            if (id != contract.ContractId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(contract);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ContractExists(contract.ContractId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CustomerId"] = new SelectList(_context.Customer, "CustomerId", "CustomerId", contract.CustomerId);
+            return View(contract);
+        }
+
+        // GET: UTMContract/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contract = await _context.Contract
+                .Include(c => c.Customer)
+                .FirstOrDefaultAsync(m => m.ContractId == id);
+            if (contract == null)
+            {
+                return NotFound();
+            }
+
+            return View(contract);
+        }
+
+        // POST: UTMContract/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var contract = await _context.Contract.FindAsync(id);
+            _context.Contract.Remove(contract);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ContractExists(int id)
+        {
+            return _context.Contract.Any(e => e.ContractId == id);
+        }
+    }
+}
